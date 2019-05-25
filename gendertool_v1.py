8eaea3d4d8f76e018c118ad6cecde7e6cd02ecb4
Christopher Carnes <carnes58@gmail.com>
2019-05-25 23:49:18 +0200

    Updated script

--- gendertool_v1.py
+++ gendertool_v1.py
@@ -1,16 +1,23 @@
 import nltk
+from collections import Counter
 
 male_names = nltk.corpus.names.words("male.txt")
 fem_names = nltk.corpus.names.words("female.txt")
 
 file = input("Enter the name of your .txt file: ")
-
 open_file = open(file).read()
+tokenize_names = nltk.word_tokenize(open_file)
+
+unique_names = []
+
+for name in tokenize_names:
+    if name not in unique_names:
+        unique_names.append(name)
 
 males = []
 females = []
 
-for name in nltk.word_tokenize(open_file):
+for name in unique_names:
     if name in male_names:
         males.append(name)
     elif name in fem_names:
@@ -20,8 +27,10 @@ for name in nltk.word_tokenize(open_file):
 print(males + females)
 print("Your text contained " + str(len(males)) + " male and " + str(len(females)) + " female names.")
 
-perc_male = len(males) / len(nltk.word_tokenize(open_file)) * 100
-perc_female = len(females) / len(nltk.word_tokenize(open_file)) * 100
+all_names = males + females
 
-print(str(perc_male)[:4] + "%")
-print(str(perc_female)[:4] + "%")
+perc_male = len(males) / len(all_names) * 100
+perc_female = len(females) / len(all_names) * 100
+
+print("Male: " + str(perc_male)[:2] + "%")
+print("Female: " + str(perc_female)[:2] + "%")
