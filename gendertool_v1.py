4d6e49587025c45d0544c2ae21988da68228c9bb
Christopher Carnes <carnes58@gmail.com>
2019-05-25 22:50:33 +0200

    Updated to calculate percentage

--- gendertool_v1.py
+++ gendertool_v1.py
@@ -3,11 +3,25 @@ import nltk
 male_names = nltk.corpus.names.words("male.txt")
 fem_names = nltk.corpus.names.words("female.txt")
 
-name = input("Enter the name here: ")
+file = input("Enter the name of your .txt file: ")
 
-if name in male_names:
-    print("This is a male name.")
-elif name in fem_names:
-    print("This is a female name.")
-else:
-    print("This name was not found in the database.")
+open_file = open(file).read()
+
+males = []
+females = []
+
+for name in nltk.word_tokenize(open_file):
+    if name in male_names:
+        males.append(name)
+    elif name in fem_names:
+        females.append(name)
+
+
+print(males + females)
+print("Your text contained " + str(len(males)) + " male and " + str(len(females)) + " female names.")
+
+perc_male = len(males) / len(nltk.word_tokenize(open_file)) * 100
+perc_female = len(females) / len(nltk.word_tokenize(open_file)) * 100
+
+print(str(perc_male)[:4] + "%")
+print(str(perc_female)[:4] + "%")
