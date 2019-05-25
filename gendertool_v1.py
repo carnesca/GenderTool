5312df4f423e556b04ccb441e4aa19462cafbca2
Christopher Carnes <carnes58@gmail.com>
2019-05-25 17:04:26 +0200

    Add new gendertool.py

---
+++ gendertool_v1.py
@@ -0,0 +1,11 @@
+import nltk
+
+male_names = nltk.corpus.names.words("male.txt")
+fem_names = nltk.corpus.names.words("female.txt")name = input("Enter the name here: ")
+
+if name in male_names:
+    print("This is a male name.")
+elif name in fem_names:
+    print("This is a female name.")
+else:
+    print("This name was not found in the database.")
